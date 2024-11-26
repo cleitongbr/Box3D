99a5dea1bca26aadb0d2cdeadc0afc0fb4790adb
Cleiton <123893939+cleitongbr@users.noreply.github.com>
2024-11-26 14:48:03 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,26 +1,26 @@
 @echo off
-:: Verificar se o Python está instalado
+:: Check if Python is installed
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Python nao esta instalado ou nao foi adicionado ao PATH.
+    echo Python is not installed or has not been added to the PATH.
     pause
     exit /b
 )
 
-:: Instalar dependencias do requirements.txt
-echo Instalando dependencias...
+:: Install requirements.txt dependencies
+echo Installing dependencies...
 pip install -r requirements.txt
 if %errorlevel% neq 0 (
-    echo Falha ao instalar as dependencias do requirements.txt.
+    echo Failed to install dependencies from requirements.txt.
     pause
     exit /b
 )
 
-:: Executar o app.py
-echo Iniciando a aplicacao...
+:: start the app.py
+echo Starting the application...
 python app.py
 if %errorlevel% neq 0 (
-    echo Ocorreu um erro ao executar o app.py.
+    echo An error occurred while executing app.py
     pause
     exit /b
 )
